2f50048193fc2d96b07384ab42c6015550bb44af
yawen <sunyawen1999@gmail.com>
2025-04-16 15:49:55 -0400

    initial

---
+++ lib/message_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MessagePage extends StatelessWidget {
+  const MessagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Messages',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 70,
+      ),
+      body: Text('Message Page')
+    );
+  }
+}
